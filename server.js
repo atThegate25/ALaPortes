3136176042cf782b2aaa2afdcf55bbbe25038bdf
Khamel <khamel.mumford@gmail.com>
2018-04-24 20:15:07 -0500

    heroku do or do not there is no try3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/public"));
+    app.use(express.static("client/build/"));
 }
 
 // Use body-parser for handling form submissions
